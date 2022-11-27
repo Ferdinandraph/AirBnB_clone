6495d904e380a16dadb95f84415531e50b0c41f9
Dmiraculous <106762511+77679@users.noreply.github.com>
2022-11-27 08:44:23 -0800

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 class BaseModel:
     """
-    A class that defines all common attributes/methods for other classes
+    A class that defines all common attributes/methods for other classes 
     """
 
     def __init__(self, *args, **kwargs):
