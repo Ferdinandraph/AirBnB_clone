28e0f0dc115f8136631386a7d3e5b061b4841e1c
Dmiraculous <106762511+77679@users.noreply.github.com>
2022-11-27 08:43:31 -0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@
 """
 __init__ dunder method for the models directory
 """
-from models.engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage 
 
 
 storage = FileStorage()
